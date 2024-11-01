c4dda765b2d143fa4a40333b85a872d2b2099a30
Guilherme Dudu Berti <guiberti1227@gmail.com>
2024-11-01 19:46:54 -0300

    first commit

---
+++ Producao.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Producao {
+  private List<Receita> receitas = new ArrayList<>();
+
+  public void adicionarReceita(Receita receita) {
+    receitas.add(receita);
+  }
+
+  public void listarReceitas() {
+    for (Receita receita : this.receitas) {
+      System.out.println(receita.getNome());
+    }
+  }
+
+  public void verificarEstoque(Receita receita) {
+    for (Ingrediente ingrediente : receita.getIngredientes()) {
+      System.out.println("Verificando estoque do ingrediente " + ingrediente.getNome());
+    }
+  }
+
+  public List<Receita> getReceitas() {
+    return this.receitas;
+  }
+}
